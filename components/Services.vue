c30d742f76799d9d30dfad7b6a5c83a37a1e74d0
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-03-29 13:43:51 +1100

    add service content and styling

--- components/Services.vue
+++ components/Services.vue
@@ -6,17 +6,17 @@
           <div class="themesflat-spacer clearfix" data-desktop="60" data-mobile="60" data-smobile="60" />
           <div class="themesflat-headings style-1 text-center clearfix">
             <h2 class="heading">
-              BEST SERVICES
+              {{ services.title }}
             </h2>
-            <div class="sep has-icon width-125 clearfix">
+            <!-- <div class="sep has-icon width-125 clearfix">
               <div class="sep-icon">
                 <span class="sep-icon-before sep-center sep-solid" />
                 <span class="icon-wrap"><i class="autora-icon-build" /></span>
                 <span class="sep-icon-after sep-center sep-solid" />
               </div>
-            </div>
+            </div> -->
             <p class="sub-heading">
-              Construction Services offers a full range of construction services from initial design to project completion.
+              {{ services.intro }}
             </p>
           </div>
           <div class="themesflat-spacer clearfix" data-desktop="39" data-mobile="35" data-smobile="35" />
@@ -28,129 +28,88 @@
             data-column3="1"
             data-auto="false"
           >
-            <div class="owl-carousel owl-theme">
-              <div class="themesflat-image-box style-1 has-icon icon-right w65 clearfix">
-                <div class="image-box-item">
-                  <div class="inner">
-                    <div class="thumb data-effect-item">
-                      <img src="~/assets/img/imagebox/image-box-1-370x250.jpg" alt="Image">
-                      <div class="overlay-effect bg-color-accent" />
-                    </div>
-                    <div class="text-wrap">
-                      <h5 class="heading">
-                        <a href="#">DESIGN & BUILD</a>
-                      </h5>
-                      <span class="icon-wrap">
-                        <i class="fa fa-angle-right" />
-                      </span>
-                    </div>
-                  </div>
+            <div class="service-container row">
+              <div v-for="(service, index) in services.services" :key="index" class="card col-md-3">
+                <div class="thumb data-effect-item">
+                  <img :src="service.img" :alt="service.title" class="card-img-top">
+                  <div class="overlay-effect bg-color-accent" />
                 </div>
-              </div><!-- /.themesflat-image-box -->
-
-              <div class="themesflat-image-box style-1 has-icon icon-right w65 clearfix">
-                <div class="image-box-item">
-                  <div class="inner">
-                    <div class="thumb data-effect-item">
-                      <img src="~/assets/img/imagebox/image-box-2-370x250.jpg" alt="Image">
-                      <div class="overlay-effect bg-color-accent" />
-                    </div>
-                    <div class="text-wrap">
-                      <h5 class="heading">
-                        <a href="#">BUILDING ENVELOPES</a>
-                      </h5>
-                      <span class="icon-wrap">
-                        <i class="fa fa-angle-right" />
-                      </span>
-                    </div>
-                  </div>
+                <div class="card-body">
+                  <h5 class="card-title">
+                    {{ service.title }}
+                  </h5>
+                  <p class="card-text">
+                    {{ service.description }}
+                  </p>
+                  <a href="#" class="btn btn-link">{{ service.buttonText }}</a>
                 </div>
-              </div><!-- /.themesflat-image-box -->
-
-              <div class="themesflat-image-box style-1 has-icon icon-right w65 clearfix">
-                <div class="image-box-item">
-                  <div class="inner">
-                    <div class="thumb data-effect-item">
-                      <img src="~/assets/img/imagebox/image-box-3-370x250.jpg" alt="Image">
-                      <div class="overlay-effect bg-color-accent" />
-                    </div>
-                    <div class="text-wrap">
-                      <h5 class="heading">
-                        <a href="#">CONSTRUCTION</a>
-                      </h5>
-                      <span class="icon-wrap">
-                        <i class="fa fa-angle-right" />
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div><!-- /.themesflat-image-box -->
-
-              <div class="themesflat-image-box style-1 has-icon icon-right w65 clearfix">
-                <div class="image-box-item">
-                  <div class="inner">
-                    <div class="thumb data-effect-item">
-                      <img src="~/assets/img/imagebox/image-box-1-370x250.jpg" alt="Image">
-                      <div class="overlay-effect bg-color-accent" />
-                    </div>
-                    <div class="text-wrap">
-                      <h5 class="heading">
-                        <a href="#">DESIGN & BUILD</a>
-                      </h5>
-                      <span class="icon-wrap">
-                        <i class="fa fa-angle-right" />
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div><!-- /.themesflat-image-box -->
-
-              <div class="themesflat-image-box style-1 has-icon icon-right w65 clearfix">
-                <div class="image-box-item">
-                  <div class="inner">
-                    <div class="thumb data-effect-item">
-                      <img src="~/assets/img/imagebox/image-box-2-370x250.jpg" alt="Image">
-                      <div class="overlay-effect bg-color-accent" />
-                    </div>
-                    <div class="text-wrap">
-                      <h5 class="heading">
-                        <a href="#">BUILDING ENVELOPES</a>
-                      </h5>
-                      <span class="icon-wrap">
-                        <i class="fa fa-angle-right" />
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div><!-- /.themesflat-image-box -->
-
-              <div class="themesflat-image-box style-1 has-icon icon-right w65 clearfix">
-                <div class="image-box-item">
-                  <div class="inner">
-                    <div class="thumb data-effect-item">
-                      <img src="~/assets/img/imagebox/image-box-3-370x250.jpg" alt="Image">
-                      <div class="overlay-effect bg-color-accent" />
-                    </div>
-                    <div class="text-wrap">
-                      <h5 class="heading">
-                        <a href="#">CONSTRUCTION MANAGEMENT</a>
-                      </h5>
-                      <span class="icon-wrap">
-                        <i class="fa fa-angle-right" />
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div><!-- /.themesflat-image-box -->
+              </div>
             </div>
+            <!-- <div class="owl-carousel owl-theme">
+              <div v-for="(service, index) in services.services" :key="index" class="themesflat-image-box style-1 has-icon icon-right w65 clearfix">
+                <div class="image-box-item">
+                  <div class="inner">
+                    <div class="thumb data-effect-item">
+                      <img :src="service.img" :alt="service.title">
+                      <div class="overlay-effect bg-color-accent" />
+                    </div>
+                    <div class="text-wrap">
+                      <h5 class="heading">
+                        <a href="#">{{ service.title }}</a>
+                      </h5>
+                      <span class="icon-wrap">
+                        <i class="fa fa-angle-right" />
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div> -->
+            <!-- /.themesflat-image-box -->
+            <!-- </div> -->
           </div><!-- /.themesflat-carousel-box -->
           <div class="themesflat-spacer clearfix" data-desktop="50" data-mobile="35" data-smobile="35" />
-          <div class="elm-button text-center">
+          <!-- <div class="elm-button text-center">
             <a href="#" class="themesflat-button bg-accent">ALL SERVICES</a>
-          </div>
+          </div> -->
           <div class="themesflat-spacer clearfix" data-desktop="73" data-mobile="60" data-smobile="60" />
         </div><!-- /.col-md-12 -->
       </div><!-- /.row -->
     </div><!-- /.container -->
   </div>
 </template>
+
+<script>
+import services from '../content/services.json'
+
+export default {
+  data() {
+    return {
+      services: services
+    }
+  }
+}
+</script>
+
+<style>
+.service-container {
+  margin: 40px 0;
+}
+.service-container .card {
+  border: none;
+}
+.service-container .card-title {
+  margin-top: 25px;
+}
+.service-container .btn-link {
+  color: #FED00E;
+  font-size: 14px;
+  font-weight: 600;
+  float: right;
+}
+.service-container .btn-link:hover,
+.service-container .btn-link:focus,
+.service-container .btn-link.focus {
+  text-decoration: none;
+  box-shadow: none;
+}
+</style>
