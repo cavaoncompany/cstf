9b19a751cf711eccc39e620fa658415de5a230d1
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-04-08 15:24:51 +1000

    add page 2 residential of quote form

--- pages/quote.vue
+++ pages/quote.vue
@@ -4,7 +4,161 @@
     <div class="container">
       <div class="wrapper">
         <form></form>
-        <section id="quote-page-1">
+        <!-- SECTION 1 -->
+        <section v-if="currentPage === 1" id="quote-page-1">
+          <div
+            class="themesflat-spacer clearfix"
+            data-desktop="73"
+            data-mobile="60"
+            data-smobile="60"
+          />
+          <div class="themesflat-headings style-2 clearfix text-center">
+            <h2 class="heading">
+              {{ quote.title }}
+            </h2>
+            <div class="sep has-width w80 accent-bg clearfix" />
+          </div>
+          <div
+            class="themesflat-spacer clearfix"
+            data-desktop="73"
+            data-mobile="60"
+            data-smobile="60"
+          />
+          <div class="sep has-width w80 accent-bg clearfix" />
+          <div class="progress-bar-wrapper bg-white-column">
+            <div class="progress">
+              <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <p>Page {{ currentPage }} of {{ totalPages }}</p>
+            <p @click="nextPage()">{{ quote.next }}</p>
+          </div>
+          <div class="row bg-white-column">
+            <div class="themesflat-headings style-2 clearfix col-12">
+              <h3>{{quote.typeOfProject.title}}</h3>
+            </div>
+            <div v-for="(type, index) in quote.typeOfProject.types" :key="index" class="col-md-6">
+              <div :id="'type-' + type.type" class="project-type projectDeselected" @click="addProjectType(type, $event)">
+                <img :id="'tick-' + type.type" :src="quote.tick" alt="ticked" class="tick invisible">
+                <img :src="type.img" :alt="type.question">
+                <p>{{ type.question }}</p>
+              </div>
+            </div>
+            <div class="elm-button">
+              <a href="#" @click="getNext(2)" class="themesflat-button bg-accent">{{ quote.nextButton }}</a>
+            </div>
+            <div
+            class="themesflat-spacer clearfix"
+            data-desktop="73"
+            data-mobile="60"
+            data-smobile="60"
+          />
+          </div>
+          <div
+            class="themesflat-spacer clearfix"
+            data-desktop="73"
+            data-mobile="60"
+            data-smobile="60"
+          />
+        </section>
+        <!-- SECTION 2  residential -->
+        <section v-if="currentPage === 2 && projectType === 'residential'" id="quote-page-2">
+          <div
+            class="themesflat-spacer clearfix"
+            data-desktop="73"
+            data-mobile="60"
+            data-smobile="60"
+          />
+          <div class="themesflat-headings style-2 clearfix">
+            <h2 class="heading">
+              {{ quote.residentialProject }}
+            </h2>
+            <div class="sep has-width w80 accent-bg clearfix" />
+          </div>
+          <div
+            class="themesflat-spacer clearfix"
+            data-desktop="73"
+            data-mobile="60"
+            data-smobile="60"
+          />
+          <div class="sep has-width w80 accent-bg clearfix" />
+          <div class="progress-bar-wrapper bg-white-column">
+            <div class="progress">
+              <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <p>Page {{ currentPage }} of {{ totalPages }}</p>
+            <p @click="nextPage()">{{ quote.next }}</p>
+          </div>
+
+          <article class="bg-white-column questionnaire">
+            <h3>
+              {{ quote.profession.title }}
+            </h3>
+            <div v-for="(prof, index) in quote.profession.professions" :key="index">
+              <input
+                :id="'profession' + index"
+                v-model="profession"
+                type="radio"
+                name="profession"
+                :value="profession"
+              >
+              <label :for="'profession' + index">{{ prof }}</label><br>
+            </div>
+            
+            <h3>
+              {{ quote.projectType.title }}
+            </h3>
+            <div v-for="(proj, index) in quote.projectType.type" :key="index">
+              <input
+                :id="'project' + index"
+                v-model="type"
+                type="radio"
+                name="project"
+                :value="project"
+              >
+              <label :for="'project' + index">{{ proj }}</label><br>
+            </div>
+            <h3>
+              {{ quote.detail.title }}
+            </h3>
+            <div v-for="(det, index) in quote.detail.detail" :key="index">
+              <input
+                :id="'project' + index"
+                v-model="detail"
+                type="radio"
+                name="detail"
+                :value="detail"
+                @change="checkIfOther(detail)"
+              >
+              <label :for="'profession' + index">{{ det }}</label><br>
+            </div>
+            <input
+              v-if="otherSelected"
+              id="detail"
+              v-model="detailOther"
+              type="text"
+              placeholder="Other"
+              name="detailOther"
+              size="100"
+            >
+            <div
+            class="themesflat-spacer clearfix"
+            data-desktop="73"
+            data-mobile="60"
+            data-smobile="60"
+          />
+          <div class="elm-button">
+            <a href="#" @click="getNext(3)" class="themesflat-button bg-accent">{{ quote.nextButton }}</a>
+          </div>
+          </article>
+          <div
+            class="themesflat-spacer clearfix"
+            data-desktop="73"
+            data-mobile="60"
+            data-smobile="60"
+          />
+        </section>
+        <!-- SECTION 2  commercial -->
+        <section v-if="currentPage === 2 && projectType === 'commercial'" id="quote-page-2">
           <div
             class="themesflat-spacer clearfix"
             data-desktop="73"
@@ -26,25 +180,72 @@
           <div class="sep has-width w80 accent-bg clearfix" />
           <div class="progress-bar-wrapper bg-white-column">
             <div class="progress">
-              <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+              <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
             <p>Page {{ currentPage }} of {{ totalPages }}</p>
+            <p @click="nextPage()">{{ quote.next }}</p>
           </div>
-          <div class="row bg-white-column">
-            <div class="themesflat-headings style-2 clearfix col-12">
-              <h3>{{quote.typeOfProject.title}}</h3>
+          <article class="bg-white-column questionnaire">
+            <h3>
+              {{ quote.profession.title }}
+            </h3>
+            <div v-for="(prof, index) in quote.profession.professions" :key="index">
+              <input
+                :id="'profession' + index"
+                v-model="profession"
+                type="radio"
+                name="profession"
+                :value="profession"
+              >
+              <label :for="'profession' + index">{{ prof }}</label><br>
             </div>
-            <div v-for="(type, index) in quote.typeOfProject.types" :key="index" class="col-md-6">
-              <div :id="'type-' + type.type" class="project-type projectDeselected" @click="addProjectType(type, $event)">
-                <img :id="'tick-' + type.type" :src="quote.tick" alt="ticked" class="tick invisible">
-                <img :src="type.img" :alt="type.question">
-                <p>{{ type.question }}</p>
-              </div>
+            
+            <h3>
+              {{ quote.projectType.title }}
+            </h3>
+            <div v-for="(proj, index) in quote.projectType.type" :key="index">
+              <input
+                :id="'project' + index"
+                v-model="type"
+                type="radio"
+                name="project"
+                :value="project"
+              >
+              <label :for="'project' + index">{{ proj }}</label><br>
             </div>
-            <div class="elm-button">
-              <a href="#" @click="getNext(2)" class="themesflat-button bg-accent">{{ quote.nextButton }}</a>
+            <h3>
+              {{ quote.detail.title }}
+            </h3>
+            <div v-for="(det, index) in quote.detail.detail" :key="index">
+              <input
+                :id="'project' + index"
+                v-model="detail"
+                type="radio"
+                name="detail"
+                :value="detail"
+                @change="checkIfOther(detail)"
+              >
+              <label :for="'profession' + index">{{ det }}</label><br>
             </div>
+            <input
+              v-if="otherSelected"
+              id="detail"
+              v-model="detailOther"
+              type="text"
+              placeholder="Other"
+              name="detailOther"
+              size="100"
+            >
+            <div
+            class="themesflat-spacer clearfix"
+            data-desktop="73"
+            data-mobile="60"
+            data-smobile="60"
+          />
+          <div class="elm-button">
+            <a href="#" @click="getNext(3)" class="themesflat-button bg-accent">{{ quote.nextButton }}</a>
           </div>
+          </article>
           <div
             class="themesflat-spacer clearfix"
             data-desktop="73"
@@ -71,12 +272,17 @@ export default {
   data() {
     return {
       quote: quote,
-      currentPage: 1,
+      currentPage: 2,
       totalPages: 4,
+      otherSelected: false,
       name: this.$route.params.name,
       email: this.$route.params.email,
       company: this.$route.params.company,
-      projectType: ''
+      projectType: 'residential',
+      profession: '',
+      type: '',
+      detail: '',
+      detailOther: ''
     }
   },
   methods: {
@@ -101,6 +307,19 @@ export default {
         document.getElementById(type).classList.add('projectSelected')
         document.getElementById(tick).classList.remove('invisible')
       }
+    },
+    nextPage: function() {
+      this.currentPage++
+    },
+    previousPage: function() {
+      this.currentPage--
+    },
+    checkIfOther: function (value) {
+      if (value.toLowerCase() === 'other') {
+        this.otherSelected = true
+      } else {
+        this.otherSelected = false
+      }
     }
   }
 }
@@ -123,6 +342,9 @@ export default {
   padding-bottom: 80px;
   position: relative;
 }
+#quote .row {
+  margin: 0;
+}
 #quote .project-type:hover {
   cursor: pointer;
 }
@@ -131,6 +353,9 @@ export default {
 }
 #quote .projectDeselected {
   border: 1px solid #E5E5E5;
+}
+#quote h2 {
+  text-align: center;
 }
 #quote h3 {
   margin-top: 50px;
@@ -150,8 +375,8 @@ export default {
 #quote .elm-button {
   margin: 0 auto;
 }
-#quote h2, #quote .sep {
-  text-align: center;
+#quote .sep {
+  margin: 0 auto;
 }
 #quote .progress {
   width: 30%;
@@ -164,4 +389,31 @@ export default {
   padding-top: 10px;
   text-align: center;
 }
+#quote input[type="radio"] {
+    width: 0px;
+    margin-top: 8px;
+    vertical-align: middle;
+    visibility: hidden;
+    position: absolute;
+  }
+#quote input[type=radio] + label:before {
+    height:12px;
+    width:12px;
+    margin-right: 2px;
+    content: " ";
+    display:inline-block;
+    vertical-align: baseline;
+    border:1px solid #E2E2E2;
+    border-radius:50%;
+    margin-right: 10px;
+  }
+  input[type=radio]:checked + label:before {
+    background:#FFC716;
+  }
+  #quote .questionnaire {
+    padding: 2% 15%;
+  }
+  #quote .questionnaire h3 {
+    text-align: left;
+  }
 </style>
