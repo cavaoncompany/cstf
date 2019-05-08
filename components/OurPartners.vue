aa65cccffdbcb0198ce7a3a44f739e963971f2c2
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-05-08 15:48:19 +1000

    update our partner section on mobile

--- components/OurPartners.vue
+++ components/OurPartners.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="getAQuote" class="row-quote bg-row-1">
-    <div class="container">
+    <div class="container desktop-only">
       <div class="row">
         <div class="col-md-12">
           <div class="themesflat-spacer clearfix" data-desktop="40" data-mobile="60" data-smobile="60" />
@@ -20,6 +20,18 @@
         </div><!-- /.col-md-12 -->
       </div><!-- /.row -->
     </div><!-- /.container -->
+    <div class="mobile-only partner-wrapper">
+      <div class="top-row">
+        <div class="container">
+          <h3 class="heading">
+            {{ quote.startYourProject }}
+          </h3>
+        </div>
+      </div>
+      <div class="bottom-row">
+        <img src="/img/blank.png" alt="placeholder">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -45,5 +57,20 @@ export default {
 }
 .row-quote .themesflat-button.small {
   padding-right: 46px;
+}
+.partner-wrapper {
+  flex-direction: column;
+}
+.partner-wrapper .top-row {
+  background: #FED00E;
+  width: 100%;
+  text-align: center;
+  font-size: 12px;
+}
+.partner-wrapper .bottom-row {
+  height: 100px;
+}
+.partner-wrapper .top-row h3 {
+  margin-top: 15px;
 }
 </style>
