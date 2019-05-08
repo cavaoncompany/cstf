34bec011574cdee18239fcd37dd6ce780a23bdba
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-05-08 15:13:59 +1000

    update banner carousel

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,17 +1,40 @@
 <template>
-  <div class="banner-row parallax parallax-3 parallax-overlay">
-    <div class="container">
-      <div class="row">
-        <div class="banner-content col-md-9 col-lg-6">
-          <p>{{ banner.topLine }}</p>
-          <h1>{{ banner.bottomLine }}</h1>
-          <div class="elm-button">
-            <a href="#" class="themesflat-button bg-accent" data-toggle="modal" data-target="#getAQuoteModal">{{ banner.buttonText }}</a>
+  <div class="banner-row">
+    <div id="banner-carousel-desktop" class="carousel slide desktop-only" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li v-for="(slide, i) in banner.slides" :key="i" data-target="#banner-carousel-desktop" data-slide-to="i" />
+      </ol>
+      <div class="carousel-inner">
+        <div v-for="(slide, i) in banner.slides" :key="i" :class="{'active': i === 0}" class="carousel-item">
+          <img class="d-block w-100" :src="slide.image" :alt="slide.title">
+          <div class="overlay-black" />
+        </div>
+      </div>
+    </div>
+    <div id="banner-carousel-mobile" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li v-for="(slide, i) in banner.slides" :key="i" data-target="#banner-carousel-mobile" data-slide-to="i" />
+      </ol>
+      <div class="carousel-inner mobile-only">
+        <div v-for="(slide, i) in banner.slides" :key="i" :class="{'active': i === 0}" class="carousel-item">
+          <img class="d-block w-100" :src="slide.mobileImage" :alt="slide.title">
+          <div class="overlay-black" />
+        </div>
+      </div>
+    </div>
+    <div class="b-row">
+      <div class="container">
+        <div class="row">
+          <div class="banner-content col-md-9 col-lg-6">
+            <p>{{ banner.topLine }}</p>
+            <h1>{{ banner.bottomLine }}</h1>
+            <div class="elm-button">
+              <a href="#" class="themesflat-button bg-accent" data-toggle="modal" data-target="#getAQuoteModal">{{ banner.buttonText }}</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="bg-parallax-overlay overlay-black" />
   </div>
 </template>
 
@@ -28,18 +51,30 @@ export default {
 </script>
 
 <style>
+.b-row {
+  position: absolute;
+  top: 0;
+  width: 100%;
+}
 .banner-row .container {
-    padding-bottom: 50px;
-    min-height: 500px;
+  padding-bottom: 50px;
+  min-height: 500px;
 }
 .banner-row .container p {
-    color: #ffffff;
+  color: #ffffff;
 }
 .banner-row .container h1 {
-    color: #ffffff;
-    margin-bottom: 30px;
+  color: #ffffff;
+  margin-bottom: 30px;
 }
 .banner-row .themesflat-button {
-    color: #111;
+  color: #111;
+}
+.banner-row .overlay-black {
+  background-color: rgba(1, 1, 1, 0.5);
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
